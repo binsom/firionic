0fa4a0b740663635a51bcce685950df7b7e4a751
binsom <18300163780@163.com>
2018-06-19 16:00:47 +0800

    上拉(鼠标向下滚)加载更多数据

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,9 @@ import {BinoneProvider} from "../../providers/binone/binone";
 })
 export class HomePage {
   binheros:any;
-  news:[];
+  news = [];
+
+  items = [];
 
   constructor(public navCtrl: NavController,public binoneProvider:BinoneProvider) {
 
@@ -31,9 +33,26 @@ export class HomePage {
     this.binoneProvider.newsTech().subscribe(res => {
         // console.log(res);
         this.news = res.data;
-        console.log(this.news,666);
+        console.log(this.news,999)
         // console.log(this.news.data[0].media_name,666);
       });
   }
+
+  /* 加载更多 */
+  loadMore(loadEvent) {
+    console.log(111);
+    this.binoneProvider.newsTech().subscribe(res => {
+      console.log(res.data,555);
+      let len = res.data.length;
+      for(var i=0;i<len;i++){
+        this.news.push(res.data[i]);
+      }
+      //这个loadMore()必须结束掉才能出发下一次loadMore() ；
+      //一旦请求完成之后，$event.complete()手动结束
+      loadEvent.complete();
+      console.log(this.news,666);
+    });
+}
+
 
 }
