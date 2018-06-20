dbc66ed4f7f68b0e8e638baf7a744d02ac120f77
binsom <18300163780@163.com>
2018-06-20 18:48:38 +0800

    取到城市数据

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {CityProvider} from "../../providers/binone/city";
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  public hotCitys:any = [];
+  public groupCitys:any = [];
+  public finCity:any = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public cityProvider:CityProvider) {
 
+  }
+
+  ngOnInit(){
+    this.showCitys();
+    this.hot();
+  }
+
+  showCitys(){
+    this.cityProvider.getCity('group').subscribe(res => {
+      // console.log(res,'数据')
+      for(var key in res){
+          this.groupCitys.push(key);
+      }
+      this.groupCitys.sort();
+      for(let i=0;i<this.groupCitys.length;i++){
+        this.finCity.push({'sort':this.groupCitys[i],'cont':res[this.groupCitys[i]]})
+      }
+    });
+  }
+
+  hot(){
+    this.cityProvider.getCity('hot').subscribe(res => {
+       this.hotCitys = res;
+    });
   }
 
 }
